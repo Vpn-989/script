035c8b74edb2a221b7e363164768b8638e198ce5
MuLuu09 <muluu09@gmail.com>
2017-06-04 20:29:50 +0800

    Update webmin.sh

--- webmin.sh
+++ webmin.sh
@@ -3,7 +3,7 @@
 #install webmin
 cd
 #wget -O webmin-current.deb http://prdownloads.sourceforge.net/webadmin/webmin_1.760_all.deb
-wget -O webmin-current.deb $source/debian7/webmin-current.deb
+wget -O webmin-current.deb http://raw.github.com/mulzs/new/debian7/webmin-current.deb
 dpkg -i --force-all webmin-current.deb
 apt-get -y -f install;
 #sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
